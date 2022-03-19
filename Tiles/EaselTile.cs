dbc8cceccf224e277c97c88456c2d5a5dfc92170
Rijam <jacobrmoreno@hotmail.com>
2022-03-19 15:42:47 -0600

    [1.4] Updated to v1.2

--- Tiles/EaselTile.cs
+++ Tiles/EaselTile.cs
@@ -27,7 +27,7 @@ namespace CraftablePaintings.Tiles
 
 		public override void KillMultiTile(int i, int j, int frameX, int frameY)
 		{
-			Item.NewItem(i * 16, j * 16, 32, 48, ModContent.ItemType<Items.EaselItem>());
+			Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 48, ModContent.ItemType<Items.EaselItem>());
 		}
 	}
 }
